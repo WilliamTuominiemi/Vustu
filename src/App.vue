da0a24e99542aca6307b3b63b72e43924b4f2485
williamtuominiemi <william.m.tuominiemi@gmail.com>
2025-07-24 23:40:06 +0300

    controlling video

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+const videoSrc = ref<HTMLVideoElement | null>(null);
+
+function playVideo() {
+  videoSrc.value?.play();
+}
+
+function pauseVideo() {
+  videoSrc.value?.pause();
+}
+
+function goTo(seconds: number) {
+  if (videoSrc.value) {
+    videoSrc.value.currentTime = seconds;
+  }
+}
+</script>
 
 <template>
   <main>
-    <video controls width="600">
+    <video ref="videoSrc" controls width="600">
       <source src="/video.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
+    <div style="margin-top: 1rem">
+      <button @click="playVideo()">Play</button>
+      <button @click="pauseVideo()">Pause</button>
+      <button @click="goTo(2)">Go to 2s</button>
+    </div>
   </main>
 </template>
 
